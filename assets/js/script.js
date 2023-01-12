c50ba4cfec5d11eafc70ab840a53908da7a68059
Alex Nash <Alexr.nash@gmail.com>
2023-01-12 13:51:59 -0500

    cleaned up conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,30 +57,7 @@ const sNews = {
 
 // autocomplete function
 $( function() {
-  var availableTags = [
-    "ActionScript",
-    "AppleScript",
-    "Asp",
-    "BASIC",
-    "C",
-    "C++",
-    "Clojure",
-    "COBOL",
-    "ColdFusion",
-    "Erlang",
-    "Fortran",
-    "Groovy",
-    "Haskell",
-    "Java",
-    "JavaScript",
-    "Lisp",
-    "Perl",
-    "PHP",
-    "Python",
-    "Ruby",
-    "Scala",
-    "Scheme"
-  ];
+  
   $(".input").autocomplete({
     source: availableTags
   });
