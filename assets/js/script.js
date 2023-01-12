2c46792e3898e49d9b2dd9cb223ddb250d194eba
Alex Nash <Alexr.nash@gmail.com>
2023-01-12 13:09:49 -0500

    cleaned up some html and got autocomplete ready for API

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,32 +56,11 @@ const sNews = {
         .catch(err => console.error(err));
 
 // autocomplete function
+
+
 $( function() {
-  var availableTags = [
-    "ActionScript",
-    "AppleScript",
-    "Asp",
-    "BASIC",
-    "C",
-    "C++",
-    "Clojure",
-    "COBOL",
-    "ColdFusion",
-    "Erlang",
-    "Fortran",
-    "Groovy",
-    "Haskell",
-    "Java",
-    "JavaScript",
-    "Lisp",
-    "Perl",
-    "PHP",
-    "Python",
-    "Ruby",
-    "Scala",
-    "Scheme"
-  ];
+  
   $(".input").autocomplete({
-    source: availableTags
+    source: 
   });
 } );
